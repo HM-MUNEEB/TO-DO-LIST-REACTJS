a7ceccd7e2c38e893e818b918a5ebcd9a1df41e5
HM Muneeb <hm.muneeb.u.r@gmail.com>
2020-09-20 17:24:18 +0500

    Updates

---
+++ precache-manifest.c8f58277044e76cff6538ef3c5ed71de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db676ca96d0421a17972f603da95563f",
+    "url": "/TO-DO-LIST-REACTJS/index.html"
+  },
+  {
+    "revision": "0fadf36cabff7284cede",
+    "url": "/TO-DO-LIST-REACTJS/static/css/main.eeca6dd1.chunk.css"
+  },
+  {
+    "revision": "22d17d1a0e365fa09123",
+    "url": "/TO-DO-LIST-REACTJS/static/js/2.fe68eac3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TO-DO-LIST-REACTJS/static/js/2.fe68eac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fadf36cabff7284cede",
+    "url": "/TO-DO-LIST-REACTJS/static/js/main.29f114df.chunk.js"
+  },
+  {
+    "revision": "8fba7f3099106def7d45",
+    "url": "/TO-DO-LIST-REACTJS/static/js/runtime-main.9f4eae1b.js"
+  },
+  {
+    "revision": "a5135f2ff35ee56d26494fe2e8c37eef",
+    "url": "/TO-DO-LIST-REACTJS/static/media/skull_image.a5135f2f.png"
+  }
+]);
